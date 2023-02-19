ceffb4ee33569d2fce7d882ef5f1e90f312edb70
positivehun <jihun3104@gmail.com>
2023-02-19 16:10:16 +0900

    bodycator_INIT

---
+++ bodycator/src/main/java/bodycator/bodycator/BodycatorApplication.java
@@ -0,0 +1,13 @@
+package bodycator.bodycator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodycatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodycatorApplication.class, args);
+	}
+
+}
---
+++ bodycator/src/test/java/bodycator/bodycator/BodycatorApplicationTests.java
@@ -0,0 +1,13 @@
+package bodycator.bodycator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodycatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
